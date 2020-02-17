c94546a503ba9fd5356ea90dc3dab67064fcaa75
alex6446 <alexeymedenitskiy@gmail.com>
2020-02-17 11:33:34 +0200

    refactoring

--- C++/OpenGL/HelloWorld/Source/main.cpp
+++ C++/OpenGL/HelloWorld/Source/main.cpp
@@ -3,17 +3,6 @@
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
-/*#include <glm/glm.hpp>
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-#include <glm/vec2.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <SOIL2/SOIL2.h>*/
-
 #include <iostream>
 
 int main(void)

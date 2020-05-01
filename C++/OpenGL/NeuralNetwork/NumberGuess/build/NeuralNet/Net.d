bc1961b64bcf647738435d2fdec39ca1e4c4dbab
alex6446 <alexeymedenitskiy@gmail.com>
2020-05-02 02:49:03 +0300

    reset gitignore

--- C++/OpenGL/NeuralNetwork/NumberGuess/build/NeuralNet/Net.d
+++
@@ -1,6 +0,0 @@
-build/NeuralNet/Net.o: src/NeuralNet/Net.cpp include/NeuralNet/Net.h \
- include/NeuralNet/Neuron.h
-
-include/NeuralNet/Net.h:
-
-include/NeuralNet/Neuron.h:
--- C++/OpenGL/NeuralNetwork/NumberGuess/build/NeuralNet/Neuron.d
+++
@@ -1,4 +0,0 @@
-build/NeuralNet/Neuron.o: src/NeuralNet/Neuron.cpp \
- include/NeuralNet/Neuron.h
-
-include/NeuralNet/Neuron.h:
